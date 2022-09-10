08c5599698be4bfb227155d2d9286db4440c5c53
braverokmc79 <braverokmc79@gmail.com>
2022-09-10 15:47:46 +0900

    11. Favorite Button 만들기 (3)

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from 'antd';
 import Axios from 'axios';
 
@@ -10,33 +10,47 @@ function Favorite(props) {
     const moviePost = props.movieInfo.backdrop_path;
     const movieRunTime = props.movieInfo.runtime;
 
-    useEffect(() => {
+    const [FavoriteNumber, setFavoriteNumber] = useState(0);
+    const [Favorited, setFavorited] = useState(false);
 
+
+    useEffect(() => {
         let variables = {
-            // userFrom: userFrom,
-            movieId: movieId,
+            userFrom: userFrom,
+            movieId: movieId
             // movieTitle: movieTitle,
             // moviePost: moviePost,
             // movieRunTime: movieRunTime
         }
 
-        console.log("variables : ", variables);
-
         Axios.post('/api/favorite/favoriteNumber', variables)
             .then(res => {
                 if (res.data.success) {
                     console.log("res.data.favoriteNumber : ", res.data.favoriteNumber);
+                    setFavoriteNumber(res.data.favoriteNumber);
                 } else {
                     alert("숫자 정보를 가져오는데 실패 했습니다.");
                 }
             });
+
+
+        Axios.post('/api/favorite/favorited', variables)
+            .then(res => {
+                if (res.data.success) {
+                    console.log("res.data.favorited : ", res.data.favorited);
+                    setFavorited(res.data.favorited);
+                } else {
+                    alert("정보를 가져오는데 실패 했습니다.");
+                }
+            });
+
     }, []);
 
 
 
     return (
         <div>
-            <Button>Favorite</Button>
+            <Button>{Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}</Button>
         </div>
     )
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -7,16 +7,36 @@ const { Favorite } = require("../models/Favorite");
 router.post("/favoriteNumber", (req, res) => {
 
     //1.mongoDD 에서 movieId 에 대한 정보를 가져오기
-    Favorite.findById({ "movieId": req.body.movieId })
+    Favorite.find({ "movieId": req.body.movieId })
         .exec((err, info) => {
             if (err) return res.status(400).send(err);
 
             //2.좋아요를한  유저정보 길이 반환
             res.status(200).json({ success: true, favoriteNumber: info.length });
         })
+});
 
 
-})
+//내가 좋아를 했는지 여부 가져오기
+router.post("/favorited", (req, res) => {
+
+    //1.내가 이 영화를 Favorite 리스트에 넣었는지 정보를 DB 에서 가져오기
+    Favorite.find({ "movieId": req.body.movieId, "userFrom": req.body.userFrom })
+        .exec((err, info) => {
+            if (err) return res.status(400).send(err);
+
+            let result = false;
+            if (info.length !== 0) {
+                result = true;
+            }
+            res.status(200).json({ success: true, favorited: result });
+        });
+
+
+
+});
+
+
 
 
 module.exports = router;
